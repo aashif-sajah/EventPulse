357494a52d631a22a6449e3d677ead992347371a
Aashif Sajah <masaashifsajah@gmail.com>
2024-10-31 02:11:00 +0530

    Initial Commit

---
+++ src/test/java/com/aashif/EventPulse/EventPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashif.EventPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
