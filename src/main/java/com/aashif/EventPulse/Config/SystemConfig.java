1cbb72397dbf228a819837a3fef8a17d60a3b4ee
Aashif Sajah <masaashifsajah@gmail.com>
2024-11-01 03:27:07 +0530

    Added Ticket,SimulationService classes

--- src/main/java/com/aashif/EventPulse/Config/SystemConfig.java
+++ src/main/java/com/aashif/EventPulse/Config/SystemConfig.java
@@ -3,10 +3,12 @@ package com.aashif.EventPulse.Config;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class SystemConfig {
     private int totalTicket;
     private int ticketReleaseRate;
--- src/main/java/com/aashif/EventPulse/service/SimulationService.java
+++ src/main/java/com/aashif/EventPulse/service/SimulationService.java
@@ -1,7 +1,15 @@
 package com.aashif.EventPulse.service;
 
+
+import com.aashif.EventPulse.Config.SystemConfig;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimulationService {
+
+    public void startSimulation(SystemConfig config) {
+        System.out.println("Simulation started");
+        System.out.println(config.toString());
+    }
+
 }
