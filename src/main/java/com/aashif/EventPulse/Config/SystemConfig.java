4e692520d422f62be2705534503f605345bb4958
Aashif Sajah <masaashifsajah@gmail.com>
2024-11-01 02:28:27 +0530

    Updated the SystemConfig Class

--- src/main/java/com/aashif/EventPulse/Config/SystemConfig.java
+++ src/main/java/com/aashif/EventPulse/Config/SystemConfig.java
@@ -1,5 +1,16 @@
 package com.aashif.EventPulse.Config;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SystemConfig {
+    private int totalTicket;
+    private int ticketReleaseRate;
+    private int consumerRetrevalRate;
+    private int ticketPoolCapacity;
 
 }
