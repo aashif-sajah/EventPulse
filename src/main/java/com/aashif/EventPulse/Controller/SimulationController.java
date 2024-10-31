b6409697047d817b8edcd2f89b148229ae8aa4b3
Aashif Sajah <masaashifsajah@gmail.com>
2024-11-01 03:03:40 +0530

    Created SimulationService Class

--- src/main/java/com/aashif/EventPulse/Controller/SimulationController.java
+++ src/main/java/com/aashif/EventPulse/Controller/SimulationController.java
@@ -1,4 +1,19 @@
 package com.aashif.EventPulse.Controller;
 
+import com.aashif.EventPulse.Config.SystemConfig;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class SimulationController {
+
+
+
+    @PostMapping("/start-simulation")
+    public void getConfig(SystemConfig config)
+    {
+
+    }
 }
---
+++ src/main/java/com/aashif/EventPulse/service/SimulationService.java
@@ -0,0 +1,7 @@
+package com.aashif.EventPulse.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimulationService {
+}
