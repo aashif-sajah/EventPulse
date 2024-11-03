439e5a0806bccb3a6849bf6854bc4c84bd436147
Aashif Sajah <masaashifsajah@gmail.com>
2024-11-04 03:31:48 +0530

    Added Producer Class

---
+++ src/main/java/com/aashif/EventPulse/service/Producer.java
@@ -0,0 +1,11 @@
+package com.aashif.EventPulse.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Producer implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
