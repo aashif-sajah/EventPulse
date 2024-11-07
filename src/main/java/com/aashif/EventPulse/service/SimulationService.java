3ad219d29699734c29a6ebc57d7a1d1709193081
Aashif Sajah <masaashifsajah@gmail.com>
2024-11-08 03:11:22 +0530

    Updated ServiceClass

--- src/main/java/com/aashif/EventPulse/service/SimulationService.java
+++ src/main/java/com/aashif/EventPulse/service/SimulationService.java
@@ -2,19 +2,44 @@ package com.aashif.EventPulse.service;
 
 
 import com.aashif.EventPulse.Config.SystemConfig;
+import com.aashif.EventPulse.repository.TicketRepo;
+import com.aashif.EventPulse.util.LoggerUtil;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SimulationService {
+public class SimulationService
+{
+    private TicketPool ticketPool;
+    private Thread producerThread;
+    private Thread consumerThread;
 
-    public void startSimulation(SystemConfig config) {
-        System.out.println("Simulation started");
-        System.out.println(config.toString());
+    private LoggerUtil logger;
+    private TicketRepo ticketRepo;
+
+    public SimulationService(LoggerUtil logger,TicketRepo ticketRepo)
+    {
+        this.logger = logger;
+        this.ticketRepo = ticketRepo;
     }
 
-    public void getConfig(SystemConfig config) {
+
+
+    public void startSimulation(SystemConfig config)
+    {
+        this.ticketPool = new TicketPool(config.getTicketPoolCapacity());
+        producerThread = new Thread(new Producer(ticketPool,logger,ticketRepo,101, config.getTicketReleaseRate()));
+        consumerThread = new Thread(new Consumer(ticketPool,logger,101, config.getConsumerRetreivalRate()));
+
+        producerThread.start();
+        consumerThread.start();
     }
 
-    public void stopSimulation() {
+    public void stopSimulation()
+    {
+        if(producerThread.isAlive() || consumerThread.isAlive() )
+        {
+            producerThread.interrupt();
+            consumerThread.interrupt();
+        }
     }
 }
