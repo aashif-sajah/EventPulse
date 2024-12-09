eba69ae6dbb89853ba4e8a50b0323446bac74fb5
Aashif Sajah <masaashifsajah@gmail.com>
2024-12-10 02:37:18 +0530

    Added InitialConfig component

---
+++ frontend/src/app/initialConfig/initial-config/initial-config.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-config',
+  standalone: true,
+  imports: [],
+  templateUrl: './initial-config.component.html',
+  styleUrl: './initial-config.component.css'
+})
+export class InitialConfigComponent {
+
+}
