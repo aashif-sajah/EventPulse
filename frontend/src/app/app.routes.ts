b7aa402979a565564d73f6de475d03a12ed447b3
Aashif Sajah <masaashifsajah@gmail.com>
2024-12-10 03:34:22 +0530

    implemented initialconfig and mainpage logic

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage/mainpage.component';
+import { InitialConfigComponent } from './initialConfig/initial-config/initial-config.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'mainpage',
+    component: MainpageComponent
+  },
+  {
+    path: '',
+    redirectTo: '/initialConfig',
+    pathMatch: 'full'
+  },
+  {
+    path: 'initialConfig',
+    component: InitialConfigComponent
+  }
+];
--- frontend/src/app/mainpage/mainpage/mainpage.component.ts
+++ frontend/src/app/mainpage/mainpage/mainpage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mainpage',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './mainpage.component.css'
 })
 export class MainpageComponent {
+  constructor(private router: Router) { }
 
+  onBtnClick() {
+    alert('Button clicked');
+    this.router.navigate(['']);
+  }
 }
