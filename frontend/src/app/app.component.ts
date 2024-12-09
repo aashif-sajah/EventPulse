e5bb69da188e9cc7b5941a3f2cb6f1f4f85d4ca0
Aashif Sajah <masaashifsajah@gmail.com>
2024-12-10 02:56:40 +0530

    merged stuff

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormComponent } from './landing-page/form/form.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
